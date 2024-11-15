454921c4735a52914e70a67db89110e94a96fa92
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-11-15 16:39:10 +0100

    rendu final

--- helper.py
+++ helper.py
@@ -1,53 +1,76 @@
-import uuid
 import copy
+import uuid
 from datetime import datetime
-import sys
-import seaborn as sns
+
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm
+
 
 def get_id() -> int:
-  return uuid.uuid4().int
+    return uuid.uuid4().int
+
 
 def get_rollback_time_version(deltatable, version: int) -> int:
-  tmp = copy.deepcopy(deltatable)
-  start = datetime.now()
-  tmp.rollback_version(version)
-  end = datetime.now()
-  return (end - start).total_seconds() * 1000
+    tmp = copy.deepcopy(deltatable)
+    start = datetime.now()
+    tmp.rollback_version(version)
+    end = datetime.now()
+    return (end - start).total_seconds() * 1000
+
 
 def get_rollback_time_transactions(deltatable, number: int) -> int:
-  tmp = copy.deepcopy(deltatable)
-  start = datetime.now()
-  tmp.rollback_transactions(number)
-  end = datetime.now()
-  return (end - start).total_seconds() * 1000
+    tmp = copy.deepcopy(deltatable)
+    start = datetime.now()
+    tmp.rollback_transactions(number)
+    end = datetime.now()
+    return (end - start).total_seconds() * 1000
+
+
+def get_rollback_time_checkpoint(deltatable, number: int) -> int:
+    tmp = copy.deepcopy(deltatable)
+    start = datetime.now()
+    tmp.rollback_checkpoint(number)
+    end = datetime.now()
+    return (end - start).total_seconds() * 1000
 
 
 def insert_10_000_rows(deltatable):
-  sizes = []
-  for idx in tqdm(range(1_000)):
-    deltatable.insert({"name": "lucian", "role": "adc", "region": "runeterra", "species": "human"})
-    sizes.append({
-      'transactions': idx,
-      'version mode': sys.getsizeof(deltatable.get_versions()),
-      'logs mode': sys.getsizeof(deltatable.get_transactions())
-    })
-  rollback_time = []
-  for idx in tqdm(np.arange(start=1, stop=1_000, step=100)):
-    rollback_time.append({
-      'transactions': idx,
-      'version mode': get_rollback_time_version(deltatable, idx),
-      'logs mode': get_rollback_time_transactions(deltatable, idx)
-    })
-  size_df = pd.DataFrame(sizes).set_index('transactions')
-  rollback_time_df = pd.DataFrame(rollback_time).set_index('transactions')
-  return size_df, rollback_time_df
+    sizes = []
+    for idx in tqdm(range(1_000)):
+        deltatable.insert(
+            {"name": "lucian", "role": "adc", "region": "runeterra", "species": "human"}
+        )
+        sizes.append(
+            {
+                "transactions": idx,
+                "version mode": deltatable.get_size_of_versions(),
+                "logs mode": deltatable.get_size_of_transactions(),
+                "checkpoint mode": deltatable.get_size_of_checkpoints(),
+            }
+        )
+    rollback_time = []
+    for idx in tqdm(np.arange(start=1, stop=1_000, step=100)):
+        rollback_time.append(
+            {
+                "transactions": idx,
+                "version mode": get_rollback_time_version(deltatable, idx),
+                "logs mode": get_rollback_time_transactions(deltatable, idx),
+                "checkpoint mode": get_rollback_time_checkpoint(deltatable, idx),
+            }
+        )
+    size_df = pd.DataFrame(sizes).set_index("transactions")
+    rollback_time_df = pd.DataFrame(rollback_time).set_index("transactions")
+    return size_df, rollback_time_df
+
 
 def measure_rollback_time_and_size(deltatable):
-  size_df, rollback_time_df = insert_10_000_rows(deltatable)
-  fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
-  sns.lineplot(data=size_df, ax=axes[0]).set(title='Size', ylabel='bytes')
-  sns.lineplot(data=rollback_time_df, ax=axes[1]).set(title='Rollback time', ylabel='ms')
-
+    size_df, rollback_time_df = insert_10_000_rows(deltatable)
+    fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
+    sns.lineplot(data=size_df, ax=axes[0]).set(title="Size", ylabel="bytes")
+    sns.lineplot(data=rollback_time_df, ax=axes[1]).set(
+        title="Rollback time", ylabel="ms"
+    )
+    fig.savefig("perf.png")
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from deltatable import DeltaTable
-import pandas as pd
+from helper import measure_rollback_time_and_size
+
 
 def main():
     data = [
@@ -10,22 +11,38 @@ def main():
     deltatable = DeltaTable(data)
 
     # Insert
-    id = deltatable.insert({"name": "jinx", "role": "adc", "region": "shurima", "species": "human"})
+    id = deltatable.insert(
+        {"name": "jinx", "role": "adc", "region": "shurima", "species": "human"}
+    )
+    print(deltatable._db)
     # Update
-    deltatable.update(id=id, row={"name": "jinx", "role": "adc", "region": "zaun", "species": "human"})
+    deltatable.update(
+        id=id, row={"name": "jinx", "role": "adc", "region": "zaun", "species": "human"}
+    )
     # Delete
     deltatable.delete(id=deltatable.select("name == 'teemo'").index[0])
+    print(deltatable._db)
+    deltatable.rollback_last_transaction()
+    print(deltatable._db)
+    deltatable.rollback_last_transaction()
+    print(deltatable._db)
+    deltatable.rollback_last_transaction()
+    print(deltatable._db)
 
     final_data = [
         {"name": "lucian", "role": "adc", "region": "runeterra", "species": "human"},
         {"name": "nasus", "role": "top", "region": "shurima", "species": "dog"},
         {"name": "jinx", "role": "adc", "region": "zaun", "species": "human"},
     ]
+    measure_rollback_time_and_size(deltatable)
     try:
-        assert deltatable == DeltaTable(final_data), "The final data should be equal to the expected data"
+        assert deltatable == DeltaTable(
+            final_data
+        ), "The final data should be equal to the expected data"
         print("Success!")
     except AssertionError as err:
         print("Assertion failed:", err)
+
 
 if __name__ == "__main__":
     main()
